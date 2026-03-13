764029ad5e31bcf6e7eced8391733ac39aa966ae
Nick-Twardeski <ntwardeski@fingerlakes.edu>
2026-03-13 20:26:55 +0000

    try1

--- coin_toss.py
+++ coin_toss.py
@@ -1,23 +1,49 @@
 # FILE NAME - coin_toss.py
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+
+# NAME: Nick Twardeski
+# DATE: 3/13/26
+# BRIEF DESCRIPTION:  Coin Toss Lab CH4
+
+
+
 # 1. Make sure you fill out the comments above
 # 2. Write your code in the proper spot
 # 3. Be sure to answer the Reflection Questions and Attestation below
 # 4. The Sample Output has been included in this code for your convenience
+
+
+
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+# Don't forget to import random!!!!!
+import random
+
+def main():
+    coin_toss()
+
+def coin_toss():
+    
+    randomnum = random.randint(1, 100)
+
+    print ('===== Coin Flipper =====')
+
+    if randomnum >= 51:
+        print('Tails')
+    
+    else:
+        print('Heads')
 
 
 
 
 
-
-
+main()
 
 
 ########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
 
 
 ########################################
@@ -46,22 +72,8 @@ Tails
 
 
 
+Finding out I had to use >= instead of =>
 
 
 
-
-'''
-
-########################################
-#            ATTESTATION
-########################################
-'''
-It is critical in this class that you understand the concepts as we explore them because
-those concepts are required understanding for entry level programming. Reliance on resources
-like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
-may impede your understanding. Please rate how well you understand the concepts in this lab: 
-[ ] I understand very little about this lab.
-[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
-[ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
 '''
